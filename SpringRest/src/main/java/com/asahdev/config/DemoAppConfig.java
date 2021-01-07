5c405f4fbd914ad59c4bb88f3240cb498ccd44d4
Ashish Sahdev <ashishsahdev15@yahoo.in>
2021-01-07 16:05:47 +0530

    Updates

---
+++ SpringRest/src/main/java/com/asahdev/config/DemoAppConfig.java
@@ -0,0 +1,13 @@
+package com.asahdev.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.asahdev")
+public class DemoAppConfig implements WebMvcConfigurer {
+
+}
