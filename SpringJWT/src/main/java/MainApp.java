bd14d9c84b227ad63efb3ff38144c540e3ea1473
Ashish Sahdev <ashishsahdev15@yahoo.in>
2021-02-17 16:00:40 +0530

    Spring JWT

---
+++ SpringJWT/src/main/java/MainApp.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan( basePackages = {"config", "service", "controller"})
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
